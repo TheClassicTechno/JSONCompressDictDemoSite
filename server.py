8c042104e0bc10b414af1f94144ba0bbc9d2522c
TheClassicTechno <jhuang999263@apple.com>
2025-07-15 23:33:13 -0700

    made edits to view delta.json correctly in localhost, now made working zstd dict compression demo site

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ def cache_response(resp, max_age=3600):
     """Add strong caching headers to a response."""
     now = datetime.utcnow()
     expire_time = now + timedelta(seconds=max_age)
-    resp.headers['Cache-Control'] = f'public, max-age={max_age}'
+    resp.headers['Cache-Control'] = 'public, max-age={}'.format(max_age)
     resp.headers['Expires'] = expire_time.strftime("%a, %d %b %Y %H:%M:%S GMT")
     resp.headers['Date'] = now.strftime("%a, %d %b %Y %H:%M:%S GMT")
     return resp
@@ -22,14 +22,29 @@ def base_json():
     resp = make_response(send_from_directory('.', 'base.json'))
     return cache_response(resp, max_age=86400)  # 1 day cache
 
+@app.route("/delta.json")
+def delta_json():
+    resp = make_response(send_from_directory('.', 'delta.json'))
+    return cache_response(resp, max_age=3600)
+
+
 @app.route("/delta.json.zst")
 def delta_json_zst():
     with open('delta.json.zst', 'rb') as f:
         compressed_data = f.read()
-    resp = Response(compressed_data, mimetype='application/json')
-    resp.headers['Content-Encoding'] = 'zstd'
-    resp.headers['Use-As-Dictionary'] = 'match="base.json"'  # required by browser spec
-    return cache_response(resp, max_age=3600)  # 1 hour cache
+    resp = Response(compressed_data, mimetype='application/octet-stream')
+    # Do NOT set Content-Encoding
+    resp.headers['Use-As-Dictionary'] = 'match="base.json"'
+    return cache_response(resp, max_age=3600)
+
+# @app.route("/delta.json.zst")
+# def delta_json_zst():
+#     with open('delta.json.zst', 'rb') as f:
+#         compressed_data = f.read()
+#     resp = Response(compressed_data, mimetype='application/json')
+#     resp.headers['Content-Encoding'] = 'zstd'
+#     resp.headers['Use-As-Dictionary'] = 'match="base.json"'  # required by browser spec
+#     return cache_response(resp, max_age=3600)  # 1 hour cache
 
 if __name__ == "__main__":
     app.run(debug=True)
